ae8be87ec5116caeb8222be663305fb745a081dc
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-02 10:41:20 -0700

    needs save instead of needs commit

--- include/bm_configs_generic.h
+++ include/bm_configs_generic.h
@@ -20,8 +20,7 @@ typedef struct ConfigKey {
 
 const ConfigKey *bcmp_config_get_stored_keys(uint8_t &num_stored_keys);
 bool bcmp_remove_key(const char *key, size_t key_len);
-// TODO - converge on commit vs save as the naming convention!
-bool bcmp_config_needs_commit(void);
+bool bcmp_config_needs_save(void);
 bool bcmp_save_config(bool restart = true);
 bool bcmp_set_config(const char *key, size_t key_len, uint8_t *value,
                      size_t value_len);
