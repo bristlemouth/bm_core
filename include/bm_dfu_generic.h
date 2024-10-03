3891e6d65e9c2808b21a52e4e4cc11e10260eba4
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-03 14:15:29 -0700

    add the dfu_confirm stuff (+8 squashed commits) Squashed commits: [56b14a9] revert test/cmakelists.txt changes since it still broke... not sure what is going on since I didn't change anything [a31056f] remove the commented out copy pbuf code, get rid guide comments, try to fix test build [a6b24c0] I don't think we should be allocating another pbuf here [fad0f3e] have the get size return the size [058e8f6] actually probably don't need this [0de4974] add another function that since we need to abstract all access's to the flash area pointer [18748e6] likely need to have these as void * so we can pass arbitrary data into it. [4f2e7fe] add the functions that we will need to implement

--- include/bm_dfu_generic.h
+++ include/bm_dfu_generic.h
@@ -1,7 +1,13 @@
 #include "util.h"
+#include <stdint.h>
 
-
-// TODO - add the dfu functions that we will need... need to reference the dfu code to see what we need
-// write
-// read
-// erase
+BmErr bm_dfu_set_confirmed(void);
+BmErr bm_dfu_set_pending_and_reset(void);
+BmErr bm_dfu_fail_update_and_reset(void);
+BmErr bm_dfu_flash_area_open(const void **flash_area);
+BmErr bm_dfu_flash_area_close(const void *flash_area);
+BmErr bm_dfu_flash_area_write(const void *flash_area, uint32_t off, const void *src, uint32_t len);
+BmErr bm_dfu_flash_area_erase(const void *flash_area, uint32_t off, uint32_t len);
+uint32_t bm_dfu_flash_area_get_size(const void *flash_area);
+bool bm_dfu_confirm_is_enabled(void);
+void bm_dfu_confirm_enable(bool en);
