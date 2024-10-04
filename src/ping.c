faaa3087ea714cd457651c499dde0f5fa41503ff
Matthew Krause <kraus2mj@gmail.com>
2024-10-04 11:11:51 -0700

    Adds Ping Unit Test

--- src/ping.c
+++ src/ping.c
@@ -6,16 +6,16 @@
 #include <inttypes.h>
 #include <string.h>
 
-static uint64_t PING_REQUEST_TIMEOUT;
-static uint32_t BCMP_SEQ;
-static uint8_t *EXPECTED_PAYLOAD = NULL;
-static uint16_t EXPECTED_PAYLOAD_LEN = 0;
+static uint64_t ping_request_timeout;
+static uint32_t bcmp_seq;
+static uint8_t *expected_payload = NULL;
+static uint16_t expected_payload_len = 0;
 
 /*!
   @brief Send ping to node(s)
 
   @param node - node id to send ping to (0 for all nodes)
-  @param *addr - ip address to send to send ping request to
+  @param *addr - ip address to send ping request to
   @param *payload - payload to send with ping
   @param payload_len - length of payload to send
 
@@ -31,32 +31,30 @@ BmErr bcmp_send_ping_request(uint64_t node, const void *addr,
   }
 
   uint16_t echo_len = sizeof(BcmpEchoRequest) + payload_len;
-  uint8_t *echo_req_buff = (uint8_t *)bm_malloc(echo_len);
+  BcmpEchoRequest *echo_req = (BcmpEchoRequest *)bm_malloc(echo_len);
 
-  if (echo_req_buff) {
-    memset(echo_req_buff, 0, echo_len);
-
-    BcmpEchoRequest *echo_req = (BcmpEchoRequest *)echo_req_buff;
+  if (echo_req) {
+    memset(echo_req, 0, echo_len);
 
     echo_req->target_node_id = node;
     echo_req->id =
         (uint16_t)node_id(); // TODO - make this a randomly generated number
-    echo_req->seq_num = BCMP_SEQ++;
+    echo_req->seq_num = bcmp_seq++;
     echo_req->payload_len = payload_len;
 
     // clear the expected payload
-    if (EXPECTED_PAYLOAD != NULL) {
-      bm_free(EXPECTED_PAYLOAD);
-      EXPECTED_PAYLOAD = NULL;
-      EXPECTED_PAYLOAD_LEN = 0;
+    if (expected_payload != NULL) {
+      bm_free(expected_payload);
+      expected_payload = NULL;
+      expected_payload_len = 0;
     }
 
     // Lets only copy the payload if it isn't NULL just in case
     if (payload != NULL && payload_len > 0) {
       memcpy(&echo_req->payload[0], payload, payload_len);
-      EXPECTED_PAYLOAD = (uint8_t *)bm_malloc(payload_len);
-      memcpy(EXPECTED_PAYLOAD, payload, payload_len);
-      EXPECTED_PAYLOAD_LEN = payload_len;
+      expected_payload = (uint8_t *)bm_malloc(payload_len);
+      memcpy(expected_payload, payload, payload_len);
+      expected_payload_len = payload_len;
     }
 
     printf("PING (%016" PRIx64 "): %" PRIu16 " data bytes\n",
@@ -65,9 +63,9 @@ BmErr bcmp_send_ping_request(uint64_t node, const void *addr,
     err = bcmp_tx(addr, BcmpEchoRequestMessage, (uint8_t *)echo_req, echo_len,
                   0, NULL);
 
-    PING_REQUEST_TIMEOUT = bm_ticks_to_ms(bm_get_tick_count());
+    ping_request_timeout = bm_ticks_to_ms(bm_get_tick_count());
 
-    bm_free(echo_req_buff);
+    bm_free(echo_req);
   }
 
   return err;
@@ -101,14 +99,15 @@ static BmErr bcmp_send_ping_reply(BcmpEchoReply *echo_reply, void *addr,
 */
 static BmErr bcmp_process_ping_request(BcmpProcessData data) {
   BcmpEchoRequest *echo_req = (BcmpEchoRequest *)data.payload;
+  BmErr err = BmOK;
   if ((echo_req->target_node_id == 0) ||
       (node_id() == echo_req->target_node_id)) {
     echo_req->target_node_id = node_id();
-    return bcmp_send_ping_reply((BcmpEchoReply *)echo_req, data.dst,
-                                echo_req->seq_num);
+    err = bcmp_send_ping_reply((BcmpEchoReply *)echo_req, data.dst,
+                               echo_req->seq_num);
   }
 
-  return BmOK;
+  return err;
 }
 
 /*!
@@ -126,31 +125,23 @@ static BmErr bcmp_process_ping_reply(BcmpProcessData data) {
   BmErr err = BmEINVAL;
   BcmpEchoReply *echo_reply = (BcmpEchoReply *)data.payload;
 
-  do {
-    if (EXPECTED_PAYLOAD_LEN != echo_reply->payload_len) {
-      break;
-    }
-
-    // TODO - once we have random numbers working we can then use a static number to check
-    if ((uint16_t)node_id() != echo_reply->id) {
-      break;
-    }
-
-    if (EXPECTED_PAYLOAD != NULL) {
-      if (memcmp(EXPECTED_PAYLOAD, echo_reply->payload,
+  // TODO - once we have random numbers working we can then use a static number to check
+  if (expected_payload_len == echo_reply->payload_len &&
+      ((uint16_t)node_id() == echo_reply->id)) {
+    if (expected_payload != NULL) {
+      if (memcmp(expected_payload, echo_reply->payload,
                  echo_reply->payload_len) != 0) {
-        break;
+        return err;
       }
     }
 
-    uint64_t diff = bm_ticks_to_ms(bm_get_tick_count()) - PING_REQUEST_TIMEOUT;
+    uint64_t diff = bm_ticks_to_ms(bm_get_tick_count()) - ping_request_timeout;
     printf("🏓 %" PRIu16 " bytes from %016" PRIx64 " bcmp_seq=%" PRIu32
            " time=%" PRIu64 " ms\n",
            echo_reply->payload_len, echo_reply->node_id, echo_reply->seq_num,
            diff);
     err = BmOK;
-
-  } while (0);
+  }
 
   return err;
 }
---
+++ test/src/ping_test.cpp
@@ -0,0 +1,112 @@
+#include <gtest/gtest.h>
+#include <helpers.hpp>
+
+#include "fff.h"
+
+DEFINE_FFF_GLOBALS;
+
+extern "C" {
+#include "messages/ping.h"
+#include "mock_bcmp.h"
+#include "mock_bm_os.h"
+#include "mock_device.h"
+#include "mock_packet.h"
+}
+
+class ping_test : public ::testing::Test {
+public:
+  rnd_gen RND;
+
+private:
+protected:
+  ping_test() {}
+  ~ping_test() override {}
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(ping_test, request_ping) {
+  BcmpEchoRequest request = {
+      (uint64_t)RND.rnd_int(UINT64_MAX, UINT8_MAX),
+      (uint16_t)RND.rnd_int(UINT16_MAX, UINT8_MAX),
+      (uint16_t)RND.rnd_int(UINT16_MAX, UINT8_MAX),
+      0,
+  };
+  BcmpProcessData data = {0};
+  data.payload = (uint8_t *)&request;
+
+  // Initialize processing callbacks
+  ASSERT_EQ(ping_init(), BmOK);
+
+  // Test successful request process
+  bcmp_tx_fake.return_val = BmOK;
+  node_id_fake.return_val = request.target_node_id;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoRequestMessage, data), BmOK);
+  RESET_FAKE(bcmp_tx);
+
+  // Test request process with failure to transmit
+  bcmp_tx_fake.return_val = BmEBADMSG;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoRequestMessage, data), BmEBADMSG);
+  ASSERT_EQ(bcmp_tx_fake.call_count, 1);
+  RESET_FAKE(bcmp_tx);
+  RESET_FAKE(node_id);
+
+  // Test request process with wrong node id
+  node_id_fake.return_val = 0;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoRequestMessage, data), BmOK);
+  ASSERT_EQ(bcmp_tx_fake.call_count, 0);
+  RESET_FAKE(node_id);
+
+  // Test request process with target id of 0
+  bcmp_tx_fake.return_val = BmOK;
+  node_id_fake.return_val = request.target_node_id;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoRequestMessage, data), BmOK);
+  ASSERT_EQ(bcmp_tx_fake.call_count, 1);
+  RESET_FAKE(node_id);
+  RESET_FAKE(bcmp_tx);
+
+  packet_cleanup();
+}
+
+TEST_F(ping_test, reply_ping) {
+  const char *str = "Ping Test String";
+  BcmpEchoReply *reply =
+      (BcmpEchoReply *)bm_malloc(sizeof(BcmpEchoRequest) + strlen(str));
+  BcmpEchoRequest request = {
+      (uint64_t)RND.rnd_int(UINT64_MAX, UINT8_MAX),
+      (uint16_t)RND.rnd_int(UINT16_MAX, UINT8_MAX),
+      (uint16_t)RND.rnd_int(UINT16_MAX, UINT8_MAX),
+      0,
+  };
+  BcmpProcessData data = {0};
+  memcpy(&reply->payload[0], str, strlen(str));
+  reply->id = request.id;
+  reply->node_id = request.target_node_id;
+  reply->seq_num = request.seq_num;
+  reply->payload_len = strlen(str);
+  data.payload = (uint8_t *)reply;
+
+  // Initialize processing callbacks
+  ASSERT_EQ(ping_init(), BmOK);
+
+  // Test proper use case
+  node_id_fake.return_val = request.id;
+  ASSERT_EQ(bcmp_send_ping_request(request.target_node_id, NULL,
+                                   (const uint8_t *)str, strlen(str)),
+            BmOK);
+  bm_ticks_to_ms_fake.return_val = 100;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoReplyMessage, data), BmOK);
+  RESET_FAKE(bm_ticks_to_ms)
+
+  // Run again to free mem
+  ASSERT_EQ(bcmp_send_ping_request(request.target_node_id, NULL,
+                                   (const uint8_t *)str, strlen(str)),
+            BmOK);
+  bm_ticks_to_ms_fake.return_val = 100;
+  ASSERT_EQ(packet_process_invoke(BcmpEchoReplyMessage, data), BmOK);
+  RESET_FAKE(bm_ticks_to_ms)
+
+  //
+
+  free(reply);
+}
