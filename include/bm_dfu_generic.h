a31056f76d723c56ba81f47cf25d0151921b9535
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-04 10:36:15 -0700

    remove the commented out copy pbuf code, get rid guide comments, try to fix test build

--- include/bm_dfu_generic.h
+++ include/bm_dfu_generic.h
@@ -1,16 +1,6 @@
 #include "util.h"
 #include <stdint.h>
 
-// These are all the flash / mcu boot functions that are called in bcmp_dfu_client.cpp
-// FLASH_AREA_IMAGE_SECONDARY --> this is called as an input into the flash_area_open function, so i think we can put it into the bm_generic_flash_area_open function
-// boot_set_confirmed
-// boot_set_pending
-// flash_area_close
-// flash_area_erase
-// flash_area_get_size
-// flash_area_open
-// flash_area_write
-
 BmErr bm_dfu_set_confirmed(void);
 BmErr bm_dfu_set_pending_and_reset(void);
 BmErr bm_dfu_fail_update_and_reset(void);
