db8713c9cb51bb0be3e5109dfcfe71f4f3658f39
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-04 17:09:56 -0700

    actually leave this one how it was

--- include/bm_dfu_generic.h
+++ include/bm_dfu_generic.h
@@ -10,5 +10,5 @@ BmErr bm_dfu_client_flash_area_write(const void *flash_area, uint32_t off, const
 BmErr bm_dfu_client_flash_area_erase(const void *flash_area, uint32_t off, uint32_t len);
 uint32_t bm_dfu_client_flash_area_get_size(const void *flash_area);
 bool bm_dfu_client_confirm_is_enabled(void);
-void bm_dfu_client_confirm_enable(bool reset);
+void bm_dfu_client_confirm_enable(bool en);
 BmErr bm_dfu_host_get_chunk(uint32_t offset, uint8_t *buffer, size_t len, uint32_t timeouts);
