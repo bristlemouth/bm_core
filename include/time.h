fc12541264d1d06a45791e8e0fdf9dce9d299e28
Victor Sowa <victor.sowa@sofarocean.com>
2024-10-11 13:08:37 -0700

    copy the time files over

---
+++ include/time.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include "util.h"
+
+BmErr bcmp_time_set_time(uint64_t target_node_id, uint64_t utc_us);
+BmErr bcmp_time_get_time(uint64_t target_node_id);
+BmErr time_init(void);
