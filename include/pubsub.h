01c593e3ca75d403a958ef49bd8a4a9c53a6ffe9
Matthew Krause <kraus2mj@gmail.com>
2024-10-21 10:46:08 -0700

    Adds Middleware Files To bm_core
    Moves middleware and pubsub files

---
+++ include/pubsub.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef BM_COMMON_PUB_SUB_VERSION
+#define BM_COMMON_PUB_SUB_VERSION (2)
+#endif // BM_COMMON_PUB_SUB_VERSION
+
+#define BM_TOPIC_MAX_LEN (255)
+
+// Add data structures published through pub sub must have this header.
+typedef struct {
+  uint8_t type;       // Type of data.
+  uint8_t version;    // Protocol Version.
+  uint8_t payload[0]; // Payload
+} __attribute__((packed)) BmPubSubHeader;
+
+typedef void (*BmPubSubCb)(uint64_t node_id, const char *topic,
+                           uint16_t topic_len, const uint8_t *data,
+                           uint16_t data_len, uint8_t type, uint8_t version);
+
+bool bm_pub(const char *topic, const void *data, uint16_t len, uint8_t type,
+            uint8_t version);
+bool bm_pub_wl(const char *topic, uint16_t topic_len, const void *data,
+               uint16_t len, uint8_t type, uint8_t version);
+bool bm_sub(const char *topic, const BmPubSubCb callback);
+bool bm_sub_wl(const char *topic, uint16_t topic_len,
+               const BmPubSubCb callback);
+bool bm_unsub(const char *topic, const BmPubSubCb callback);
+bool bm_unsub_wl(const char *topic, uint16_t topic_len,
+                 const BmPubSubCb callback);
+void bm_handle_msg(uint64_t node_id, void *buf, uint32_t size);
+void bm_print_subs(void);
+char *bm_get_subs(void);
+
+#ifdef __cplusplus
+}
+#endif
