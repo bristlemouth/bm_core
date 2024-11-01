d42ef9d17b63e2e6c34c44cdac496d8e2402a560
Matthew Krause <kraus2mj@gmail.com>
2024-11-01 15:22:40 -0700

    Update generic configs file for new API

--- bcmp/bm_configs_generic.h
+++ bcmp/bm_configs_generic.h
@@ -6,4 +6,10 @@
 #include "util.h"
 #include <stdint.h>
 
+bool bm_config_read(BmConfigPartition partition, uint32_t offset,
+                    uint8_t *buffer, size_t length, uint32_t timeout_ms);
+bool bm_config_write(BmConfigPartition partition, uint32_t offset,
+                     uint8_t *buffer, size_t length, uint32_t timeout_ms);
+void bm_config_reset(void);
+
 #endif // __BM_CONFIGS_GENERIC_H__
