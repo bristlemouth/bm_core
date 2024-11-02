228b7f5df6aff09987b560bb3e3cbe22f38d8b45
Zachary Crockett <zachary.crockett@sofarocean.com>
2024-11-01 17:00:26 -0700

    Rename NetworkInterface to NetworkDevice (#35)
    This avoids conflicts with lwip's netif and prevents confusion

--- network/network_device.h
+++ network/network_device.h
@@ -4,16 +4,16 @@ typedef struct {
   void (*power)(bool on);
   void (*link_change)(uint8_t port_index, bool is_up);
   size_t (*receive)(uint8_t port_index, uint8_t *data, size_t length);
-} NetworkInterfaceCallbacks;
+} NetworkDeviceCallbacks;
 
 typedef struct {
   BmErr (*const send)(void *self, uint8_t *data, size_t length,
                       uint8_t port_mask);
   BmErr (*const enable)(void *self);
   BmErr (*const disable)(void *self);
-} NetworkInterfaceTrait;
+} NetworkDeviceTrait;
 
 typedef struct {
   void *self;
-  NetworkInterfaceTrait const *trait;
-} NetworkInterface;
+  NetworkDeviceTrait const *trait;
+} NetworkDevice;
