d79f77ead28be53c41e05d65f52e0846f3b502b1
Victor Sowa <victor.sowa@sofarocean.com>
2024-11-22 15:59:52 -0800

    change up the wording a bit

--- bcmp/packet.c
+++ bcmp/packet.c
@@ -462,10 +462,12 @@ BmErr process_received_message(void *payload, uint32_t size) {
         // Utilize parsing callback
         if (cfg->process && (err = cfg->process(data)) != BmOK) {
           if (err == BmENOTINTREC) {
-            bm_debug("Received a message of type: %d that is not intended for us\n", data.header->type);
+            bm_debug("Ignored a message of type: %d since it was not intended "
+                     "for us!\n",
+                     data.header->type);
           } else {
             bm_debug("Error processing parsed cb: %d of message %d\n", err,
-                   data.header->type);
+                     data.header->type);
           }
         }
       }
