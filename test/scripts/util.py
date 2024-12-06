6e04437afd50daf977b64c0dc3637e578f8e9583
Matthew Krause <kraus2mj@gmail.com>
2024-12-05 16:37:02 -0800

    Adds time HIL test
    Tests added for setting/getting test from node

---
+++ test/scripts/util.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class RunOrder(Enum):
+    NEIGHBORS_TEST_RUN_ORDER = 1
+    TOPOLOGY_TEST_RUN_ORDER = 2
+
+
+def format_node_id_to_hex_str(self, node: int) -> str:
+    """Topology format node to hex string
+
+    This returns a formatted string that can be used to pass into
+    a terminal command to a Bristlemouth node's console.
+
+    Args:
+        node (int): A node that is obtained from the get method,
+                    or another node found elsewhere (ex. neighbor).
+    """
+    return hex(node)[2:]
