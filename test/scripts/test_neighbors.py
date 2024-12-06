27b1f2ed94e202a565fd7dad26265329779e76b4
Matthew Krause <kraus2mj@gmail.com>
2024-12-05 16:16:28 -0800

    Adds topology HIL test
    Updates run order so that neighbors must run first and then topology
    Adds read until regex method to SerialHelper class

--- test/scripts/test_neighbors.py
+++ test/scripts/test_neighbors.py
@@ -1,20 +1,24 @@
-import pytest
 from serial_helper import SerialHelper
 from neighbors_helper import Neighbors
+import pytest
 
 
 class TestNeighbors:
     """Neighbors HIL test class"""
 
+    NEIGHBORS_TEST_RUN_ORDER = 1
+
+    @pytest.mark.order(NEIGHBORS_TEST_RUN_ORDER)
     def test_neighbors_get(self, ser: SerialHelper):
         """Test to see if neighbors exist
 
         Attempts to obtain the neighbors to the node
         and asserts that they do exist.
+        This test should run first before any other tests.
 
         Args:
             ser (SerialHelper): Serial helper instance passed in from
-                                starting the pytests (see: conftest.py)
+                                starting the pytests (see: conftest.py).
         """
         port = 0
         neighbors = Neighbors(ser)
@@ -28,5 +32,5 @@ class TestNeighbors:
 
             # Ensure that a neighbor node ID exists and the port found
             # matches the port expected number
-            assert isinstance(neighbor_info[0], int)
-            assert neighbor_info[1] == port
+            if not isinstance(neighbor_info[0], int) or neighbor_info[1] != port:
+                pytest.fail("Failed neighbor test and cannot move on")
