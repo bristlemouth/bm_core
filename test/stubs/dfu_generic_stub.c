47dcd0b8f5be0ba7ce9b5700512fc07b344e2c17
Victor Sowa <85895527+victorsowa12@users.noreply.github.com>
2024-12-06 13:15:22 -0800

    Fix/remove dfu confirm from generic (#63)
    * move the dfu confirm functions from the generic file to the dfu client file
    
    * update the dfu unit tests
    
    * clang format

--- test/stubs/dfu_generic_stub.c
+++ test/stubs/dfu_generic_stub.c
@@ -5,11 +5,13 @@ DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_set_pending_and_reset);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_fail_update_and_reset);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_open, const void **);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_close, const void *);
-DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_write, const void *, uint32_t, const void *, uint32_t);
-DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_erase, const void *, uint32_t, uint32_t);
-DEFINE_FAKE_VALUE_FUNC(uint32_t, bm_dfu_client_flash_area_get_size, const void *);
-DEFINE_FAKE_VALUE_FUNC(bool, bm_dfu_client_confirm_is_enabled);
-DEFINE_FAKE_VOID_FUNC(bm_dfu_client_confirm_enable, bool);
-DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_host_get_chunk, uint32_t, uint8_t *, size_t, uint32_t);
+DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_write, const void *,
+                       uint32_t, const void *, uint32_t);
+DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_client_flash_area_erase, const void *,
+                       uint32_t, uint32_t);
+DEFINE_FAKE_VALUE_FUNC(uint32_t, bm_dfu_client_flash_area_get_size,
+                       const void *);
+DEFINE_FAKE_VALUE_FUNC(BmErr, bm_dfu_host_get_chunk, uint32_t, uint8_t *,
+                       size_t, uint32_t);
 DEFINE_FAKE_VOID_FUNC(bm_dfu_core_lpm_peripheral_active);
 DEFINE_FAKE_VOID_FUNC(bm_dfu_core_lpm_peripheral_inactive);
