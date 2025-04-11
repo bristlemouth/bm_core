552097e6c61ea466e561987ee3f0a3d79f9f0f6f
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2025-04-11 14:12:30 -0700

    Updates bm_common_messages reference and prevents double free on an item (#85)
    Updates bm_common_messages reference and prevents double free on an item

--- network/bm_lwip.c
+++ network/bm_lwip.c
@@ -346,8 +346,10 @@ void bm_l2_free(void *buf) { pbuf_free((struct pbuf *)buf); }
 /*!
   @brief Submit A Buffer Up Bristlemouth Stack
 
-  @details This shall handle transmitted the L2 (raw) data up to
-           the bcmp and middleware implementations
+  @details This shall handle transmitting the L2 (raw) data up to
+           the bcmp and middleware implementations. L2 is responsible for
+           freeing any memory that might have been allocated if this function
+           returns an error.
 
   @param buf buffer created with bm_l2_new
   @param size size of buffer to submite up the stack in bytes
@@ -362,7 +364,7 @@ BmErr bm_l2_submit(void *buf, uint32_t size) {
   // need for additional locking
   if (CTX.netif->input((struct pbuf *)buf, CTX.netif) != ERR_OK) {
     err = BmEBADMSG;
-    bm_l2_free(buf);
+    // Do not need to free memory here as it is handled in L2
   }
 
   return err;
