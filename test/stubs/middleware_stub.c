831132b67032008f1f15bd7e6b83bab468699588
Matthew Krause <kraus2mj@gmail.com>
2026-02-11 13:33:42 -0800

    Add scalable way to test invoking middleware rx callbacks

--- test/stubs/middleware_stub.c
+++ test/stubs/middleware_stub.c
@@ -1,3 +1,4 @@
+#include "ll.h"
 #include "mock_middleware.h"
 
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_middleware_rx, uint16_t, void *, uint64_t,
@@ -5,28 +6,31 @@ DEFINE_FAKE_VALUE_FUNC(BmErr, bm_middleware_rx, uint16_t, void *, uint64_t,
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_middleware_init);
 DEFINE_FAKE_VALUE_FUNC(BmErr, bm_middleware_net_tx, uint16_t, void *, uint32_t);
 
-static BmMiddlewareRxCb cb = NULL;
+static LL applications;
 
 BmErr bm_middleware_add_application(uint16_t port, BmIpAddr dest,
                                     BmMiddlewareRxCb rx_cb) {
-  (void)port;
   (void)dest;
 
-  if (!rx_cb) {
-    return BmEINVAL;
+  LLItem *item = NULL;
+  item = ll_create_item(item, &rx_cb, sizeof(BmMiddlewareRxCb), port);
+  if (!item) {
+    return BmENOMEM;
   }
 
-  cb = rx_cb;
-
-  return BmOK;
+  return ll_item_add(&applications, item);
 }
 
 void bm_middleware_invoke_cb(uint16_t port, uint64_t node_id, void *buf,
                              uint32_t size) {
-  (void)port;
+  BmMiddlewareRxCb *cb = NULL;
 
-  if (cb) {
-    printf("Here?\n");
-    cb(node_id, buf, size);
+  BmErr err = ll_get_item(&applications, port, (void **)&cb);
+  if (err != BmOK) {
+    return;
+  }
+
+  if (cb && *cb) {
+    (*cb)(node_id, buf, size);
   }
 }
