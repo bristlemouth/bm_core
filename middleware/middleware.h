88fc7ec1fae95e8b297ed49cd7e4d726bfd32941
Matthew Krause <kraus2mj@gmail.com>
2026-02-17 12:07:56 -0800

    Introduce Packet Forwarding Logic
    Introduces packet forwarding logic in L2.
    Adds MAVLink forwarding logic, currently forwards all packets to:
    
    - ports that are online
    - ports that are not the ingress port

--- middleware/middleware.h
+++ middleware/middleware.h
@@ -9,10 +9,13 @@
 #endif
 
 typedef void (*BmMiddlewareRxCb)(uint64_t node_id, void *buf, uint32_t size);
+typedef bool (*BmMiddlewareRoutingCb)(uint8_t ingress_port,
+                                      uint16_t *egress_ports, BmIpAddr *src);
 
 BmErr bm_middleware_rx(uint16_t port, void *buf, uint64_t node_id,
                        uint32_t size);
 BmErr bm_middleware_init(void);
 BmErr bm_middleware_add_application(uint16_t port, BmIpAddr dest,
-                                    BmMiddlewareRxCb rx_cb);
+                                    BmMiddlewareRxCb rx_cb,
+                                    BmMiddlewareRoutingCb routing_cb);
 BmErr bm_middleware_net_tx(uint16_t port, void *buf, uint32_t size);
--- network/l2.h
+++ network/l2.h
@@ -14,6 +14,32 @@ extern "C" {
 
 typedef void (*L2LinkChangeCb)(uint8_t port, bool state);
 
+/*!
+ @brief Handle Routing Of Link-Local Packets
+
+ @details Per Bristlemouth specification in section 5.4.4.3, this is used to
+          route Bristlemouth packets to other ports and determine if packets
+          need to be handled beyond L2. 
+          If egress_mask is 0 when this function returns, the packet is not
+          forwarded.
+          If the callback function returns true, the packet is sent up the
+          Bristlemouth stack and handled by the specific application bound
+          to the destination IP address
+
+ @param ingress_port Port packet was received on
+ @param egress_mask Mask of ports the packet should be forwarded to,
+ @param src Source IP address of packet
+ @param dest Destination IP address of packet 
+
+ @return true if the packet should be submitted through the
+              ip stack to the targeted application
+         false if the packet should not be handled and
+               just forwarded
+ */
+typedef bool (*L2LinkLocalRoutingCb)(uint8_t ingress_port,
+                                     uint16_t *egress_mask, BmIpAddr *src,
+                                     const BmIpAddr *dest);
+
 BmErr bm_l2_handle_device_interrupt(void);
 BmErr bm_l2_link_output(void *buf, uint32_t length);
 void bm_l2_deinit(void);
@@ -23,6 +49,7 @@ bool bm_l2_get_port_state(uint8_t port);
 uint8_t bm_l2_get_port_count(void);
 BmErr bm_l2_netif_set_power(bool on);
 BmErr bm_l2_netif_enable_disable_port(uint8_t port_num, bool enable);
+BmErr bm_l2_register_link_local_routing_callback(L2LinkLocalRoutingCb cb);
 
 #ifdef __cplusplus
 }
