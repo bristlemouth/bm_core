9c3f86b7a2ab1e3f14bd568363c62cd1164e233c
Matthew Krause <61891357+matt001k@users.noreply.github.com>
2026-02-17 16:10:04 -0800

    Add MAVLink Middleware Application (#115)
    Adds MAVLink middleware application bound to UDP port 14540 and sending messages on link-local unicast address FF02::3.

---
+++ middleware/bm_mavlink.h
@@ -0,0 +1,29 @@
+#ifndef __BM_MAVLINK_H__
+#define __BM_MAVLINK_H__
+
+#include "util.h"
+#include <mavlink/common/mavlink.h>
+#include <mavlink/minimal/mavlink.h>
+#include <mavlink/standard/mavlink.h>
+
+typedef struct {
+  void (*rx_cb)(mavlink_message_t *msg, mavlink_status_t *status);
+  uint32_t msg_id;
+} BmMavLinkRxEntry;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+BmErr bm_mavlink_init(uint8_t sys_id, uint8_t comp_id, BmMavLinkRxEntry *rx_lut,
+                      uint16_t rx_lut_len);
+BmErr bm_mavlink_transmit(const mavlink_message_t *msg);
+
+uint8_t bm_mavlink_get_sys_id(void);
+uint8_t bm_mavlink_get_comp_id(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
