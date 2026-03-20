3e84e143352ab5860f305de37b6988beec7cc1b7
Matthew Krause <kraus2mj@gmail.com>
2026-03-20 09:37:41 -0700

    Implements Resource Trie For RBR

---
+++ middleware/resource_trie.h
@@ -0,0 +1,44 @@
+#include "pubsub.h"
+#include "util.h"
+
+// The maximum depth a resource trie will be if every topic segment
+// is a single character:
+//                     x/x/x/x/x......./x
+#define resource_trie_max_depth (BM_TOPIC_MAX_LEN >> 1)
+
+// Max resource key is 20 bit
+#define max_resource_id (0xFFFFF)
+#define invalid_resource_id UINT32_MAX
+
+typedef struct ResourceTrieElement {
+  struct ResourceTrieElement **children; // Children from this pattern
+  const char *segment;                   // String segment key
+  uint32_t resource_id;                  // Local resource ID
+  uint16_t port_mask; // Mask of ports which expressed interest in this resource
+  uint8_t children_count;     // Number of children
+  uint8_t local_interest : 1; // Does the node subscribe to this resource
+  uint8_t reserved : 7;       // Maintains alignment
+} ResourceTrieElement;
+
+typedef struct {
+  ResourceTrieElement **matches; // Caller-supplied array.
+  uint16_t capacity;             // Maximum results to record.
+  uint16_t count;                // Number of results recorded.
+} ResourceTrieMatchResult;
+
+typedef struct {
+  ResourceTrieElement *element;
+  const char *remaining;
+} ResourceTrieStack;
+
+typedef struct {
+  ResourceTrieElement element;
+  ResourceTrieStack stack[resource_trie_max_depth];
+} ResourceTrieRoot;
+
+BmErr resource_trie_add(ResourceTrieRoot *root, const char *topic,
+                        uint32_t resource_id, uint16_t port_mask,
+                        bool local_interest);
+BmErr resource_trie_match(ResourceTrieRoot *root, const char *topic,
+                          ResourceTrieMatchResult *result);
+BmErr resource_trie_remove(ResourceTrieRoot *root, const char *topic);
