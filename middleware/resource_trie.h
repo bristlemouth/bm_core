ad5999da3dd11f0842495e876b456a60637e069e
Matthew Krause <kraus2mj@gmail.com>
2026-04-13 15:51:37 -0700

    Separate wildcard match entries from concrete interests

--- middleware/resource_trie.h
+++ middleware/resource_trie.h
@@ -11,18 +11,23 @@
 
 // Max resource key is 20 bit
 #define max_resource_id (0xFFFFF)
-#define invalid_resource_id UINT32_MAX
+#define invalid_resource_id (0x1FFFFF)
 
 typedef struct ResourceTrieElement {
+  uint64_t resource_id : 21; // Local resource ID
+  uint64_t port_mask : 16;   // Port mask of expressed interest in resource
+  uint64_t wildcard_port_mask : 16; // Matching wildcard topic mask, are other
+                                    // nodes' wildcard interests associated
+                                    // with this concrete topic
+  uint64_t segment_length : 8;      // Segment string length in bytes
+  uint64_t local_interest : 1;      // Does the node subscribe to this resource
+  uint64_t wildcard_interest : 1;   // Matching wildcard topic interest,
+                                    // does the node have wildcards
+                                    // subscribed to this concrete resource
+  uint64_t is_wildcard : 1;         // Is a wildcard topic
   struct ResourceTrieElement *children; // Children from this pattern
   struct ResourceTrieElement *sibling;  // Next sibling under same parent
   const char *segment;                  // String segment key
-  uint32_t resource_id;                 // Local resource ID
-  uint16_t port_mask; // Mask of ports which expressed interest in this resource
-  uint8_t segment_length; // Length of segment in bytes (not including nil byte)
-  uint8_t local_interest : 1; // Does the node subscribe to this resource
-  uint8_t is_wildcard : 1;    // Is a wildcard topic
-  uint8_t reserved : 6;       // Maintains alignment
 } ResourceTrieElement;
 
 typedef struct {
